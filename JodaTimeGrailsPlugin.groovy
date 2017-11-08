e43d2f4c7a40ec9568ec6e9ff0a4eaa4d98bedbc
jukin-jerry <jerry@jukinmedia.com>
2017-11-08 00:14:32 -0800

    set version to 1.6.jukin-SNAPSHOT

--- JodaTimeGrailsPlugin.groovy
+++ JodaTimeGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import grails.plugin.jodatime.converters.JodaConverters
 
 class JodaTimeGrailsPlugin {
 
-	def version = '1.6-SNAPSHOT'
+	def version = '1.6.jukin-SNAPSHOT'
 	def grailsVersion = '2.3 > *'
 	def dependsOn = [converters: '2.0 > *']
 
