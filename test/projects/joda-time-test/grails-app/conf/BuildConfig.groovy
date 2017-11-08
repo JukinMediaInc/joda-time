dba25b451c8104d5419e484aed1cc3b31300b33d
jukin-jerry <jerry@jukinmedia.com>
2017-11-08 01:58:09 -0800

    upgrade to Grails 2.5.6: more dependency upgrades

--- test/projects/joda-time-test/grails-app/conf/BuildConfig.groovy
+++ test/projects/joda-time-test/grails-app/conf/BuildConfig.groovy
@@ -14,20 +14,31 @@ grails.project.dependency.resolution = {
 		mavenCentral()
 	}
 
-	def gebVersion = '0.13.1'
-	def seleniumVersion = '3.7.1'
+	def gebVersion = '0.9.2'
+	def seleniumVersion = '2.41.0'
 
 	dependencies {
 		test "org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion",
 				"org.seleniumhq.selenium:selenium-support:$seleniumVersion",
 				"org.gebish:geb-spock:$gebVersion",
-				"com.mashape.unirest:unirest-java:1.3.8"
+				"org.grails:grails-datastore-test-support:1.0.2-grails-2.4",
+				"com.mashape.unirest:unirest-java:1.3.8",
+				'org.hamcrest:hamcrest-all:1.3',
+				"org.jodd:jodd-lagarto:3.4.1",
+				"org.grails:grails-web-databinding-spring:$grailsVersion",
+				'org.springframework:spring-expression:4.1.9.RELEASE',
+				'org.springframework:spring-aop:4.1.9.RELEASE'
 		compile 'org.jadira.usertype:usertype.jodatime:2.0.1'
+		runtime 'org.springframework:spring-expression:4.1.9.RELEASE',
+                'org.springframework:spring-aop:4.1.9.RELEASE'
 	}
 	plugins {
 		build ":tomcat:7.0.70"
-		runtime ":fields:1.3",
+
+        //noinspection GroovyAssignabilityCheck
+        runtime ":fields:1.3",
 				":hibernate4:4.3.10"
+        //noinspection GroovyAssignabilityCheck
 		test ':build-test-data:2.4.0',
 				':fixtures:1.3',
 				":geb:$gebVersion"
--- test/projects/joda-time-test/grails-app/conf/DataSource.groovy
+++ test/projects/joda-time-test/grails-app/conf/DataSource.groovy
@@ -5,9 +5,17 @@ dataSource {
     password = ""
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    //show_sql = true
+    namingStrategy = DefaultNamingStrategy
+    cache.use_second_level_cache = false
+    cache.use_query_cache = false
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+//    cache.region.factory_class = 'grails.plugin.cache.ehcache.hibernate.BeanEhcacheRegionFactory' // Hibernate 3
+    cache.provider_class = 'grails.plugin.cache.ehcache.hibernate.GrailsEhCacheManagerFactoryBean'
+    cache.useCacheProvider = true
+    singleSession = true   // do not open a hibernate session for every request
+    flush.mode = "auto" // this is the default.  other values are "commit" and "manual"
+    generate_statistics = true
 }
 // environment specific settings
 environments {
